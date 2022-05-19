65121bd9671033a9ede78162b7718b8d9b970543
overalz <wangr7636@gmail.com>
2022-05-20 11:16:10 +1200

    Condensed the menu into a class of its own for future-proofing.

--- cafemenu.py
+++ cafemenu.py
@@ -56,54 +56,73 @@ root = tkinter.Tk()
 s = ttk.Style(root)
 root.geometry("300x500+150+200")
 
-menuCanvas = tkinter.Canvas(root)
-mainFrame = ttk.Frame(menuCanvas)
-scroll = ttk.Scrollbar(menuCanvas, orient='vertical', command=menuCanvas.yview)
-    
-mainFrame.bind(
-    "<Configure>",
-    lambda e: menuCanvas.configure(
-        scrollregion=menuCanvas.bbox("all")
-    )
-)
+#Styles
+s.configure("ItemFrame.TFrame", background="white")
+s.configure("ItemFrame.TButton", background="#99032e")
+s.configure("ItemFrame.TLabel", background="#b0ffe9")
+s.configure("ItemFrameA.TLabel", background="#99032e", foreground="#FFFFFF")
 
-window = menuCanvas.create_window((0, 0), window=mainFrame, anchor="nw")
-menuCanvas.configure(yscrollcommand=scroll.set)
 
-def FrameWidth(event):
-    canvas_width = event.width
-    menuCanvas.itemconfig(window, width = canvas_width-19)
-
-menuCanvas.bind('<Configure>', FrameWidth)
-
-mainFrameLeft = ttk.Frame(mainFrame)
-mainFrameRight = ttk.Frame(mainFrame)
 
 #Global Functions
-def fillConv(i):
+def fillConv(i, mainFrameLeft, mainFrameRight):
     i = 1-(i%2)
     if i == 0: return mainFrameLeft
     else: return mainFrameRight
 
 #Classes
-s.configure("ItemFrame.TFrame", background="white")
-s.configure("ItemFrame.TButton", background="#99032e")
-s.configure("ItemFrame.TLabel", background="#b0ffe9")
-s.configure("ItemFrameA.TLabel", background="#99032e", foreground="#FFFFFF")
+class Menu:
+    def __init__(self) -> None:
+        
+        self.menuCanvas = tkinter.Canvas(root)
+        self.mainFrame = ttk.Frame(self.menuCanvas)
+        self.scroll = ttk.Scrollbar(self.menuCanvas, orient='vertical', command=self.menuCanvas.yview)
+            
+        self.mainFrame.bind(
+            "<Configure>",
+            lambda e: self.menuCanvas.configure(
+                scrollregion=self.menuCanvas.bbox("all")
+            )
+        )
+
+        self.window = self.menuCanvas.create_window((0, 0), window=self.mainFrame, anchor="nw")
+        self.menuCanvas.configure(yscrollcommand=self.scroll.set)
+
+        self. menuCanvas.bind('<Configure>', self.FrameWidth)
+
+        self.mainFrameLeft = ttk.Frame(self.mainFrame)
+        self.mainFrameRight = ttk.Frame(self.mainFrame)
+
+        pass
+
+    def FrameWidth(self,event):
+        canvas_width = event.width
+        self.menuCanvas.itemconfig(self.window, width = canvas_width-19)
+
+    def Setup(self):
+        i = 0
+        for _ in range(1,3):
+            for Name,Info in Items.items():
+                i += 1
+                ItemFrame(Name,Info,i,self.mainFrameLeft,self.mainFrameRight)
+        self.menuCanvas.pack(fill="both", expand=True)
+        self.scroll.pack(side="right",fill="both")
+
+        self.mainFrameLeft.pack(side="left", expand=True, fill="both")
+        self.mainFrameRight.pack(side="right", expand=True, fill="both")
+
 class ItemFrame:
-    def __init__(self,name,info,i) -> None:
+    def __init__(self,name,info,i,mfl,mfr) -> None:
         #Store Arguments
         self.Info = info
         self.Name = name
 
         #Frame Style
         #Create Base Frame
-        f = fillConv(i)
+        f = fillConv(i,mfl,mfr)
         self.Frame = ttk.Frame(f, height=100, width=500, style="ItemFrame.TFrame")
         self.Frame.pack(side="top", fill="x", ipadx=10, ipady=10)
         
-        #self.Frame.grid(column=1-(i%2),row=math.ceil(i/2))
-
         label = ttk.Label(self.Frame, text=name, anchor="center", style="ItemFrame.TLabel")
         label.pack(expand=True,fill="both")
         ttk.Label(self.Frame, style="ItemFrameA.TLabel", text="$" + str(float(info["Price"]))).pack(fill="both",expand=True,side="left")
@@ -114,16 +133,7 @@ class ItemFrame:
     def f(self):
         self.obj.configure(text=self.info["ClickText"])
 
-i = 0
-for _ in range(1,3):
-    for Name,Info in Items.items():
-        i += 1
-        ItemFrame(Name,Info,i)
-
-menuCanvas.pack(fill="both", expand=True)
-scroll.pack(side="right",fill="both")
-
-mainFrameLeft.pack(side="left", expand=True, fill="both")
-mainFrameRight.pack(side="right", expand=True, fill="both")
+menu = Menu()
+menu.Setup()
 
 root.mainloop()
