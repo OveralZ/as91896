14d971bdb15138c373c45aecc693cc2689854b20
OveralZ <wangr7636@gmail.com>
2022-06-10 12:22:08 +1200

    Moved the menu to a separate file.

---
+++ menu.py
@@ -0,0 +1,42 @@
+Items = {
+    "Potato Puff": {
+        "Price": 3.5,
+        "Tier": "S",
+    },
+    "Hot Noodles": {
+        "Price": 3.5,
+        "Tier": "S+",
+    },
+    "Spaghetti Bun": {
+        "Price": 2,
+        "Tier": "B",
+    },
+    "Placeholder Bun": {
+        "Price": 2.99,
+        "Tier": "B",
+    },
+    "Placeholder Bun 2": {
+        "Price": 2,
+        "Tier": "B",
+    },
+    "Placeholder Bun 3": {
+        "Price": 2,
+        "Tier": "B",
+    },
+    "Placeholder Bun 4": {
+        "Price": 2,
+        "Tier": "B",
+    },
+    "Placeholder Bun 5": {
+        "Price": 2,
+        "Tier": "B",
+    },
+    "Placeholder Bun 6": {
+        "Price": 2,
+        "Tier": "B",
+    },
+    "Placeholder Bun 7": {
+        "Price": 2,
+        "Tier": "B",
+    },
+}
